832619eb5e8ad5683abe7287445c961d6bd94937
NikPeg <peganov.nik@gmail.com>
2023-04-10 10:40:14 +0300

    Button works

--- MnemonicSystems/app/src/main/java/ru/dewhilefor/mnemonicsystems/ui/numbers/NumbersFragment.java
+++ MnemonicSystems/app/src/main/java/ru/dewhilefor/mnemonicsystems/ui/numbers/NumbersFragment.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import ru.dewhilefor.mnemonicsystems.R;
 import ru.dewhilefor.mnemonicsystems.databinding.FragmentNumbersBinding;
 
 public class NumbersFragment extends Fragment {
@@ -23,6 +28,14 @@ public class NumbersFragment extends Fragment {
 
         binding = FragmentNumbersBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+        FloatingActionButton addButton = root.findViewById(R.id.AddButton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CardView nextCardView = root.findViewById(R.id.cardView1);
+                nextCardView.setVisibility(View.VISIBLE);
+            }
+        });
 
         return root;
     }
