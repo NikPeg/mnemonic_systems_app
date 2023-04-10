eec6df5863c44cbdfbef874d9c6cbc6984f3ea95
NikPeg <peganov.nik@gmail.com>
2023-04-10 10:46:21 +0300

    Button adds cards

--- MnemonicSystems/app/src/main/java/ru/dewhilefor/mnemonicsystems/ui/numbers/NumbersFragment.java
+++ MnemonicSystems/app/src/main/java/ru/dewhilefor/mnemonicsystems/ui/numbers/NumbersFragment.java
@@ -1,5 +1,7 @@
 package ru.dewhilefor.mnemonicsystems.ui.numbers;
 
+import static java.lang.Math.min;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,12 +16,16 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.List;
+
 import ru.dewhilefor.mnemonicsystems.R;
 import ru.dewhilefor.mnemonicsystems.databinding.FragmentNumbersBinding;
 
 public class NumbersFragment extends Fragment {
 
     private FragmentNumbersBinding binding;
+    private final int[] cards = new int[]{R.id.cardView0, R.id.cardView1, R.id.cardView2, R.id.cardView3, R.id.cardView4, R.id.cardView5, R.id.cardView6, R.id.cardView7, R.id.cardView8, R.id.cardView9};
+    private int lastCard = 0;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -32,7 +38,8 @@ public class NumbersFragment extends Fragment {
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CardView nextCardView = root.findViewById(R.id.cardView1);
+                lastCard = min(cards.length - 1, lastCard + 1);
+                CardView nextCardView = root.findViewById(cards[lastCard]);
                 nextCardView.setVisibility(View.VISIBLE);
             }
         });
