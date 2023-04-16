7d9110af55e5b9205cc346f3fce2959a0f2c7400
NikPeg <peganov.nik@gmail.com>
2023-04-16 10:38:35 +0300

    Normal code in DS

--- MnemonicSystems/app/src/main/java/ru/dewhilefor/mnemonicsystems/ui/ds/DSFragment.java
+++ MnemonicSystems/app/src/main/java/ru/dewhilefor/mnemonicsystems/ui/ds/DSFragment.java
@@ -11,11 +11,17 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import ru.dewhilefor.mnemonicsystems.R;
 import ru.dewhilefor.mnemonicsystems.databinding.FragmentDsBinding;
 
 public class DSFragment extends Fragment {
 
     private FragmentDsBinding binding;
+
+    private final String[] buttonTexts = {"A", "1", "B", "2", "C", "3", "D", "4", "E", "5", "S", "6", "G", "7", "H", "8", "N", "9", "O", "0"};
+
+    private final int[] buttonColors = {R.color.orange_red, R.color.magneta};
+    private int current_card_index = 0;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -34,66 +40,9 @@ public class DSFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 DScardButton.setTextSize(100);
-                if (DScardButton.getText() == "A") {
-                    DScardButton.setText("1");
-                }
-                else if (DScardButton.getText() == "1") {
-                    DScardButton.setText("B");
-                }
-                else if (DScardButton.getText() == "B") {
-                    DScardButton.setText("2");
-                }
-                else if (DScardButton.getText() == "2") {
-                    DScardButton.setText("C");
-                }
-                else if (DScardButton.getText() == "C") {
-                    DScardButton.setText("3");
-                }
-                else if (DScardButton.getText() == "3") {
-                    DScardButton.setText("D");
-                }
-                else if (DScardButton.getText() == "D") {
-                    DScardButton.setText("4");
-                }
-                else if (DScardButton.getText() == "4") {
-                    DScardButton.setText("E");
-                }
-                else if (DScardButton.getText() == "E") {
-                    DScardButton.setText("5");
-                }
-                else if (DScardButton.getText() == "5") {
-                    DScardButton.setText("S");
-                }
-                else if (DScardButton.getText() == "S") {
-                    DScardButton.setText("6");
-                }
-                else if (DScardButton.getText() == "6") {
-                    DScardButton.setText("G");
-                }
-                else if (DScardButton.getText() == "G") {
-                    DScardButton.setText("7");
-                }
-                else if (DScardButton.getText() == "7") {
-                    DScardButton.setText("H");
-                }
-                else if (DScardButton.getText() == "H") {
-                    DScardButton.setText("8");
-                }
-                else if (DScardButton.getText() == "8") {
-                    DScardButton.setText("N");
-                }
-                else if (DScardButton.getText() == "N") {
-                    DScardButton.setText("9");
-                }
-                else if (DScardButton.getText() == "9") {
-                    DScardButton.setText("O");
-                }
-                else if (DScardButton.getText() == "O") {
-                    DScardButton.setText("0");
-                }
-                else {
-                    DScardButton.setText("A");
-                }
+                DScardButton.setText(buttonTexts[current_card_index]); // cycle through the buttonTexts array
+                DScardButton.setBackgroundColor(getResources().getColor(buttonColors[current_card_index % 2]));
+                current_card_index = (current_card_index + 1) % buttonTexts.length;
             }
         });
 
