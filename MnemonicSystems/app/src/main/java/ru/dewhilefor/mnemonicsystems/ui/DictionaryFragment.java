d963961f8254436a21abf20fc36b8c05116c776c
NikPeg <peganov.nik@gmail.com>
2023-04-17 07:15:46 +0300

    Edit text in dict

--- MnemonicSystems/app/src/main/java/ru/dewhilefor/mnemonicsystems/ui/DictionaryFragment.java
+++ MnemonicSystems/app/src/main/java/ru/dewhilefor/mnemonicsystems/ui/DictionaryFragment.java
@@ -4,20 +4,32 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
 
 import ru.dewhilefor.mnemonicsystems.R;
+import ru.dewhilefor.mnemonicsystems.databinding.FragmentDictionaryBinding;
+import ru.dewhilefor.mnemonicsystems.databinding.FragmentMmsBinding;
+import ru.dewhilefor.mnemonicsystems.databinding.FragmentNumbersBinding;
+import ru.dewhilefor.mnemonicsystems.ui.mms.MMSViewModel;
 
 public class DictionaryFragment extends Fragment {
+    private FragmentDictionaryBinding binding;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dictionary, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_dictionary, container, false);
+        TextView textView = view.findViewById(R.id.text_dictionary);
+        textView.setText("Новый текст");
+        return view;
     }
 }
