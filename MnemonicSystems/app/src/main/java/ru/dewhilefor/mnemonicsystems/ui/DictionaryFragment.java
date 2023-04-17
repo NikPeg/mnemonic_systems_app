f4546bd70036c3c284facbc8bf68842013550997
NikPeg <peganov.nik@gmail.com>
2023-04-17 07:26:00 +0300

    Open dict file

--- MnemonicSystems/app/src/main/java/ru/dewhilefor/mnemonicsystems/ui/DictionaryFragment.java
+++ MnemonicSystems/app/src/main/java/ru/dewhilefor/mnemonicsystems/ui/DictionaryFragment.java
@@ -6,11 +6,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 import ru.dewhilefor.mnemonicsystems.R;
 import ru.dewhilefor.mnemonicsystems.databinding.FragmentDictionaryBinding;
@@ -19,7 +23,22 @@ import ru.dewhilefor.mnemonicsystems.databinding.FragmentNumbersBinding;
 import ru.dewhilefor.mnemonicsystems.ui.mms.MMSViewModel;
 
 public class DictionaryFragment extends Fragment {
-    private FragmentDictionaryBinding binding;
+    private final static String FILE_NAME = "dict.txt";
+
+    private void setDict(TextView textView) {
+        FileInputStream fin = null;
+        try {
+            fin = getActivity().openFileInput(FILE_NAME);
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String(bytes);
+            textView.setText(text);
+        } catch (NullPointerException ex) {
+            Toast.makeText(getActivity(), "There is no activity: " + ex.getMessage(), Toast.LENGTH_SHORT).show();
+        } catch (IOException ex) {
+            Toast.makeText(getActivity(), "There is no file: " + ex.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Nullable
     @Override
@@ -29,7 +48,7 @@ public class DictionaryFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_dictionary, container, false);
         TextView textView = view.findViewById(R.id.text_dictionary);
-        textView.setText("Новый текст");
+        setDict(textView);
         return view;
     }
 }
