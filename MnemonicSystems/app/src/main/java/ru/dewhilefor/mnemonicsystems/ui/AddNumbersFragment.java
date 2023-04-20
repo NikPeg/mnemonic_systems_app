d40951d1c810f57e0d993662cc16d6eb0b3c481a
NikPeg <peganov.nik@gmail.com>
2023-04-21 01:51:44 +0300

    Add a pop up to choose a system

--- MnemonicSystems/app/src/main/java/ru/dewhilefor/mnemonicsystems/ui/AddNumbersFragment.java
+++ MnemonicSystems/app/src/main/java/ru/dewhilefor/mnemonicsystems/ui/AddNumbersFragment.java
@@ -1,5 +1,6 @@
 package ru.dewhilefor.mnemonicsystems.ui;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,10 +8,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import ru.dewhilefor.mnemonicsystems.R;
+import ru.dewhilefor.mnemonicsystems.databinding.FragmentAddNumbersBinding;
 
 public class AddNumbersFragment extends Fragment {
+
+    private FragmentAddNumbersBinding binding;
 
     public AddNumbersFragment() {
         // Required empty public constructor
@@ -30,6 +37,31 @@ public class AddNumbersFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_numbers, container, false);
+        binding = FragmentAddNumbersBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        Button chooseSystemButton = binding.chooseSystemButton;
+
+        chooseSystemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new MaterialAlertDialogBuilder(requireContext())
+                        .setTitle(R.string.choose_system)
+                        .setPositiveButton(R.string.text_mms, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+//                                setLocale("");
+                            }
+                        })
+                        .setNegativeButton(R.string.text_ds, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+//                                setLocale("en");
+                            }
+                        })
+                        .show();
+            }
+        });
+        return root;
     }
 }
