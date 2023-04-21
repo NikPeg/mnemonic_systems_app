6445be893ec51a72740cd94713289db07ae9e424
NikPeg <peganov.nik@gmail.com>
2023-04-21 10:06:37 +0300

    Add onClick on button: does not work

--- MnemonicSystems/app/src/main/java/ru/dewhilefor/mnemonicsystems/ui/ChooseWordsFragment.java
+++ MnemonicSystems/app/src/main/java/ru/dewhilefor/mnemonicsystems/ui/ChooseWordsFragment.java
@@ -1,23 +1,44 @@
 package ru.dewhilefor.mnemonicsystems.ui;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import ru.dewhilefor.mnemonicsystems.R;
+import ru.dewhilefor.mnemonicsystems.databinding.FragmentAddNumbersBinding;
+import ru.dewhilefor.mnemonicsystems.databinding.FragmentChooseWordsBinding;
 
 public class ChooseWordsFragment extends Fragment {
+    private FragmentChooseWordsBinding binding;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_choose_words, container, false);
+        binding = FragmentChooseWordsBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        FloatingActionButton digit1Button = binding.Digit1Button;
+
+        digit1Button.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onClick(View v) {
+                digit1Button.setBackgroundColor(R.color.brighter_magneta);
+            }
+        });
+        return root;
     }
 }
