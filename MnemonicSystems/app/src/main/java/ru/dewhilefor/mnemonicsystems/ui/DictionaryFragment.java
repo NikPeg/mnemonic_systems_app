c9445c3ee3f68e416a91ec93b542cae0812f253e
NikPeg <peganov.nik@gmail.com>
2023-04-23 11:32:53 +0300

    Add an alert to add a word

--- MnemonicSystems/app/src/main/java/ru/dewhilefor/mnemonicsystems/ui/DictionaryFragment.java
+++ MnemonicSystems/app/src/main/java/ru/dewhilefor/mnemonicsystems/ui/DictionaryFragment.java
@@ -1,10 +1,15 @@
 package ru.dewhilefor.mnemonicsystems.ui;
 
+import static java.lang.Math.min;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,16 +18,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 
 import ru.dewhilefor.mnemonicsystems.R;
+import ru.dewhilefor.mnemonicsystems.databinding.FragmentChooseWordsBinding;
 import ru.dewhilefor.mnemonicsystems.databinding.FragmentDictionaryBinding;
 import ru.dewhilefor.mnemonicsystems.databinding.FragmentMmsBinding;
 import ru.dewhilefor.mnemonicsystems.databinding.FragmentNumbersBinding;
 import ru.dewhilefor.mnemonicsystems.ui.mms.MMSViewModel;
+import ru.dewhilefor.mnemonicsystems.ui.numbers.NumbersFragment;
 
 public class DictionaryFragment extends Fragment {
+    private FragmentDictionaryBinding binding;
     private final static String FILE_NAME = "dict.txt";
 
     private void setDict(TextView textView) {
@@ -45,10 +55,40 @@ public class DictionaryFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_dictionary, container, false);
-        TextView textView = view.findViewById(R.id.text_dictionary);
+        binding = FragmentDictionaryBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+        TextView textView = root.findViewById(R.id.text_dictionary);
         setDict(textView);
-        return view;
+
+        FloatingActionButton addButton = binding.AddButton;
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+
+                alert.setTitle("Title");
+                alert.setMessage("Message");
+
+                final EditText input = new EditText(getContext());
+                alert.setView(input);
+
+                alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        String value = String.valueOf(input.getText());
+                        // Do something with value!
+                    }
+                });
+
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // Canceled.
+                    }
+                });
+
+                alert.show();
+            }
+        });
+
+        return root;
     }
 }
